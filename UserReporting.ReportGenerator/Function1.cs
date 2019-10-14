9578f3edff925ab0b56eefd2a9ebed403aba1645
iboanta <ionut.boanta@walkerscott.co>
2019-10-14 15:59:13 +0300

    Implemented file retrieval from Azure

--- UserReporting.ReportGenerator/Function1.cs
+++ UserReporting.ReportGenerator/Function1.cs
@@ -11,78 +11,120 @@ using System.IO;
 using ImageResizer;
 using ImageResizer.Configuration;
 using ImageResizer.Plugins.PdfRenderer;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
 
 namespace UserReporting.ReportGenerator
 {
     public static class Function1
     {
+        private static readonly string _MainStorageAccountName = Environment.GetEnvironmentVariable("MainStorageAccountName");
+        private static readonly string _MainStorageAccountKey = Environment.GetEnvironmentVariable("MainStorageAccountKey");
         [FunctionName("Function1")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            Convert();
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+            var tenantId = data.TenantId;
+            var tenancyName = data.TenancyName;
+            var assetFilename = data.AssetFilename;
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            await Convert(tenantId, tenancyName, assetFilename);
 
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return req.CreateResponse(HttpStatusCode.OK);
         }
 
-        private static void Convert()
+        private static async Task Convert(long tenantId, string tenancyName, string assetFilename)
         {
+            var containerName = tenantId.ToString() + "-" + tenancyName;
+            // Retrieve reference to the blob
+            var blobPath = string.Format(@"assets/{0}", assetFilename);
+            var inputBlob = await GetBlobAsync(containerName, blobPath);
+
             PdfDocument s_document = new PdfDocument();
 
             PdfPage page = s_document.AddPage();
 
             XGraphics gfx = XGraphics.FromPdfPage(page);
 
-            XImage image = XImage.FromFile(@"D:\home\site\wwwroot\bd223fb1-c0ad-4034-8ceb-657592188f2d.tif");
-
-            page.Width = image.PointWidth;
-            page.Height = image.PointHeight;
-
-            gfx.DrawImage(image, 0, 0);
-
-            using (var stream = new MemoryStream())
+            using (var blobStream = await inputBlob.OpenReadAsync())
             {
-                s_document.Save(stream, false);
+                XImage image = XImage.FromStream(blobStream);
 
-                var thumbnailIns = new Instructions
+                page.Width = image.PointWidth;
+                page.Height = image.PointHeight;
+
+                gfx.DrawImage(image, 0, 0);
+
+                using (var stream = new MemoryStream())
                 {
-                    Width = 640,
-                    Height = 360,
-                    Mode = FitMode.Max,
-                    Page = 1,
-                    Format = "jpg"
-                };
+                    s_document.Save(stream, false);
 
-                var config = BuildApplicationResizerConfig();
-
-                using (var thumbnailStream = new MemoryStream())
-                {
-                    var thumbnailJob = new ImageJob(stream, thumbnailStream, thumbnailIns);
-                    config.Build(thumbnailJob);
-
-                    thumbnailStream.Seek(0, SeekOrigin.Begin);
-
-                    using (var fileStream = File.Create(@"D:\home\site\wwwroot\thumb.jpg"))
+                    var thumbnailIns = new Instructions
                     {
-                        thumbnailStream.CopyTo(fileStream);
+                        Width = 640,
+                        Height = 360,
+                        Mode = FitMode.Max,
+                        Page = 1,
+                        Format = "jpg"
+                    };
+
+                    var config = BuildApplicationResizerConfig();
+
+                    using (var thumbnailStream = new MemoryStream())
+                    {
+                        var thumbnailJob = new ImageJob(stream, thumbnailStream, thumbnailIns);
+                        config.Build(thumbnailJob);
+
+                        thumbnailStream.Seek(0, SeekOrigin.Begin);
+
+                        using (var fileStream = File.Create(@"D:\home\site\wwwroot\thumb.jpg"))
+                        {
+                            thumbnailStream.CopyTo(fileStream);
+                        }
                     }
                 }
             }
+           
+        }
+
+        private static async Task<CloudBlobContainer> GetBlobContainerAsync(string containerName)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount =
+            new CloudStorageAccount(new StorageCredentials(_MainStorageAccountName,
+                _MainStorageAccountKey), true);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            await container.CreateIfNotExistsAsync();
+            await container.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            return container;
+        }
+
+        private static async Task<CloudBlockBlob> GetBlobAsync(string containerName, string blobPath)
+        {
+            var path = Uri.UnescapeDataString(blobPath.Replace(containerName + "/", ""));
+            if (path.StartsWith("/"))
+                path = path.Remove(0, 1);
+
+            var container = await GetBlobContainerAsync(containerName);
+            var blob = container.GetBlockBlobReference(path);
+
+            if (!(await blob.ExistsAsync()))
+            {
+                throw new Exception("Asset not found");
+            }
+
+            return blob;
         }
 
         public static Config BuildApplicationResizerConfig()
